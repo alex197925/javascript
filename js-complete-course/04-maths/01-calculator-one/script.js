e4b7117057bb3c3f922da9cde13323253f9d0e54
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-06 15:35:48 +0200

    chapter 4.1

--- js-complete-course/04-maths/01-calculator-one/script.js
+++ js-complete-course/04-maths/01-calculator-one/script.js
@@ -12,19 +12,40 @@
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
+
+
+
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        let input1 = document.getElementById("op-one").value
+        let input2 = document.getElementById("op-two").value
+        let result = Number(input1) + Number(input2);
+        alert(result)
+
+
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let input1 = document.getElementById("op-one").value
+        let input2 = document.getElementById("op-two").value
+        let result = Number(input1) - Number(input2);
+        alert(result)
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let input1 = document.getElementById("op-one").value
+        let input2 = document.getElementById("op-two").value
+        let result = Number(input1) * Number(input2);
+        alert(result)
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let input1 = document.getElementById("op-one").value
+        let input2 = document.getElementById("op-two").value
+        let result = Number(input1) / Number(input2);
+        alert(result)
     });
 })();
