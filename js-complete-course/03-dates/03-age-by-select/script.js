6c78187363b33ae67552ffbe7eb4402764c16288
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-06 14:07:31 +0200

    chapter 3.3

--- js-complete-course/03-dates/03-age-by-select/script.js
+++ js-complete-course/03-dates/03-age-by-select/script.js
@@ -9,8 +9,33 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
     // your code here
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+//Let's see if the button get's clicked
 
-})();
+
+const btnRun = document.getElementById('run').addEventListener('click',calculateAge);
+
+
+
+function calculateAge(){
+
+    const dobDay = document.getElementById('dob-day').value;
+    const dobMonth = document.getElementById('dob-month').value;
+    const dobYear = document.getElementById('dob-year').value;
+
+    const currentDate = new Date();
+    const dobDate = new Date(dobYear,dobMonth,dobDay);
+
+    const ageYears = currentDate.getFullYear() - dobDate.getFullYear();
+
+
+    const ageMonths = currentDate.getMonth() - dobDate.getMonth() +1;
+    const ageDays = currentDate.getDay() - dobDate.getMonth() +1;
+    document.getElementById('target').innerHTML = "You are: " + ageYears ;
+
+}
+
+
