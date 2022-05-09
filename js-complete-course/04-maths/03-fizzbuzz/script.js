dac92f007817443f85b7321e869100fb1af2066a
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-09 10:11:33 +0200

    chapter 4.3

--- js-complete-course/04-maths/03-fizzbuzz/script.js
+++ js-complete-course/04-maths/03-fizzbuzz/script.js
@@ -10,7 +10,18 @@
 // You will have time to focus on it later.
 
 (function() {
-
+for(let i = 0; i <= 100; i++){
+    console.log(i)
+    if (i  %3 == 0) {
+        console.log("fizz")
+    }else if (i % 5 == 0) {
+        console.log("buzz")
+    }else if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz")
+    } else {
+        console.log(i)
+    }
+}
     // your code here
 
 })();
