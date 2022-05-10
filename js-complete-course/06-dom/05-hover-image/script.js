7c0e9d6d05a3b697deaedef9432ca1b372b08205
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-05-10 09:46:15 +0200

    chapter 6.5

--- js-complete-course/06-dom/05-hover-image/script.js
+++ js-complete-course/06-dom/05-hover-image/script.js
@@ -1,16 +1,28 @@
-/* becode/javascript
+/**
+ * /* becode/javascript
  *
  * /06-dom/05-hover-image/script.js - 6.5: survol d'image
  *
  * coded by leny@BeCode
  * started at 26/10/2018
+ *
+ * @format
  */
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+  // your code here
+ const hover = document.getElementById("img-hover").getAttribute("data-hover")
+   const sours = document.getElementById("img-hover").src
+ const image = document.getElementById("img-hover");
 
-    // your code here
+    image.onmouseover = function() {  image.src = hover; }
+    image.onmouseout = function() {  image.src = sours; }
+
+
+    console.log(sours)
+    console.log(hover)
 
 })();
